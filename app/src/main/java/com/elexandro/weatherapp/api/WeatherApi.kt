6f3364a68100834e13ae549bb0f12881571820ac
Elexandro Torres <elexandro.torres@gmail.com>
2022-09-23 15:55:34 -0300

    models and retrofit depentency injection

--- app/src/main/java/com/elexandro/weatherapp/api/WeatherApi.kt
+++ app/src/main/java/com/elexandro/weatherapp/api/WeatherApi.kt
@@ -1,7 +1,11 @@
 package com.elexandro.weatherapp.api
 
+import com.elexandro.weatherapp.model.Weather
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface WeatherApi {
 
     @GET("weather/Natal")
-    suspend fun getWeather() :
+    suspend fun getWeather() : Response<Weather>
 }
---
+++ app/src/main/java/com/elexandro/weatherapp/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.elexandro.weatherapp.di
+
+import com.elexandro.weatherapp.api.WeatherApi
+import com.elexandro.weatherapp.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : WeatherApi =
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+}
